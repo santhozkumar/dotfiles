031b15b25e85e134c1d6e641e281af54eff237e8
Santhosh Kumar Devadoss <santhoshkumardeva25@gmail.com>
2023-08-28 10:41:30 +0530

    tat function

--- install.sh
+++ install.sh
@@ -4,3 +4,18 @@ sudo apt-get install ripgrep -y
 
 # ln -s ~/dotfiles/nvim ~/.config/nvim
 # ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+#
+#
+#
+#
+# function tat {
+#   name=$(basename `pwd` | sed -e 's/\.//g')
+#
+#   if tmux ls 2>&1 | grep "$name"; then
+#     tmux attach -t "$name"
+#   elif [ -f .envrc ]; then
+#     direnv exec / tmux new-session -s "$name"
+#   else
+#     tmux new-session -s "$name"
+#   fi
+# }
