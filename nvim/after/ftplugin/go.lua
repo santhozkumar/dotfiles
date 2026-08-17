03aa145f2b58ddf82b571c05f87cd2e2bd2a52db
Santhosh Kumar Devadoss <santhoshkumardeva25@gmail.com>
2026-08-17 09:20:18 +0000

    add treesitter for js, ts, jsx

--- nvim/after/ftplugin/go.lua
+++ nvim/after/ftplugin/go.lua
@@ -9,6 +9,9 @@ require("gopher").setup()
 local ensureInstalled = {
 	"go",
 	"gomod",
+  "javascript",
+  "typescript",
+  "tsx"
 }
 local alreadyInstalled = require("nvim-treesitter.config").get_installed()
 local parsersToInstall = vim.iter(ensureInstalled)
